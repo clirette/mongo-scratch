24b51a12c9d6490c31da411d48a25b21ba677c3b
Chase Lirette <chaselirette@yahoo.com>
2019-03-22 13:02:08 -0500

    Updating

--- test/association_test.js
+++ test/association_test.js
@@ -21,12 +21,39 @@ describe('Associations', () => {
 
   });
 
-  it.only('saves a relation between user and blogpost', done => {
+  it('saves a relation between user and blogpost', done => {
     User.findOne({ name: 'Joe' })
       .populate('blogPosts')
       .then(user => {
         assert(user.blogPosts[0].title === 'JS is Great')
         done();
       });
+  });
+
+  /**
+   * populate() Perform 'joins' on models.
+   * path: the name of the thing on the document retrieved
+   * model: model of entity to populate
+   */
+  it('saves a full relation graph', done => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then(user => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is Great');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on a great post!');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+        done();
+      })
   });
 });
